33eb8b86c5336fbc1cea60b560c459403433cccb
= <=>
2024-02-05 22:17:37 +0200

    updated models

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -4,8 +4,42 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Reservation extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'reference',
+        'user_id',
+        'adults',
+        'children',
+        'status',
+        'notes',
+        'check_in',
+        'check_out',
+        'firstname',
+        'lastname',
+        'phone',
+        'email',
+        'address',
+        'city',
+        'state',
+        'zipcode',
+        'shuttle',
+        'parking',
+        'breakfast',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function rooms()
+    {
+        return $this->belongsToMany(Room::class);
+    }
+
 }
--- app/Models/Room.php
+++ app/Models/Room.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Room extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'room_number',
+        'price',
+        'description',
+        'room_type'
+    ];
+
+    public function reservations()
+    {
+        return $this->belongsToMany(Reservation::class);
+    }
 }
