b051f2c716f6373b3cac7d302a4c80a35e64940d
= <=>
2024-02-05 12:57:26 +0200

    added authentication with Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Redirect the default page to the login page
 Route::get('/', function () {
-    return view('welcome');
+    if (auth()->check()) {
+        // Redirect to dashboard if already logged in
+        return redirect()->route('dashboard');
+    }
+    // Redirect to login if not logged in
+    return redirect()->route('login');
 });
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
